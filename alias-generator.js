3493da23a0a39377fe119a01823586ab5895ad07
Josh Malkinson <josh.malkinson@gmail.com>
2015-10-06 20:17:04 +0100

    Add function to get random word

--- alias-generator.js
+++ alias-generator.js
@@ -1,4 +1,10 @@
 var adjectives = ['flawless'];
 var nouns = ['cowboy'];
 
+function getRandomElementOf(array) {
+    'use strict';
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+console.log(getRandomElementOf(nouns));
 console.log(adjectives[0] + nouns[0]);
